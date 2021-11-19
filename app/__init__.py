de7eacc0e8e9683e5873c85217a2f1f7fc22abc8
Francisco M Garcia-Moreno <fmgarmor@ugr.es>
2021-11-19 12:21:53 +0100

    refactoring comments

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,4 @@ app.config.from_object('config')
 from app import views
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000) #host visible desde internet
+    app.run(debug=False, host="0.0.0.0", port=5000) #host visible desde internet
